1337a9f789e950201e8bb5976deee528454b9c1f
Your NamePritom003 <njahanpritom65@gmail.com>
2023-11-08 17:55:22 +0600

    adde perfect style

--- src/COMPONENTS/Pages/Submissions/Allsubmission.jsx
+++ src/COMPONENTS/Pages/Submissions/Allsubmission.jsx
@@ -1,33 +1,76 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Navbar from '../../Layouts/Navbar/Navbar';
 import { Link, useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import Pdfpreview from './pdfpreview';
+
+
+
+
+
+
 
 
 const Allsubmission = () => {
   const loadeddata = useLoaderData();
 
+
+
+
+
+
+
+
+
+
   return (
     <div>
       <Navbar></Navbar>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 mx-auto justify-center align-middle items-center gap-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 mx-auto justify-center align-middle items-center gap-2">
         {loadeddata
           .filter((item) => item.type === 'pending')
           .map((item, index) => (
-            <div key={index} className="card 
-            w-96 md:w-56 md:h-[350px]  bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title">Examinee name: {item.examinee}</h2>
-                <p>Examinee email: {item.examineeemail}</p>
-                <p>Assignment type: {item.type}</p>
-                <p>Assignment name: {item.assignmentname}</p>
-                <div className="card-actions justify-end">
-                  <Link to={`/markass/${item._id}`}>
-                    <button className="btn btn-primary">Give Mark</button>
-                  </Link>
-                </div>
-              </div>
-            </div>
+            <div key={index}  className="card card-side bg-base-100 shadow-xl">
+
+
+{/* modal-------------------------------------------------------------------- */}
+
+
+  <figure>
+<label htmlFor="my_modal_6" className="btn">seepreview</label>
+<input type="checkbox" id="my_modal_6" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box">
+    <Pdfpreview item={item.link}></Pdfpreview>
+    <div className="modal-action">
+      <label htmlFor="my_modal_6" className="btn">Close!</label>
+    </div>
+  </div>
+</div></figure>
+
+
+
+
+
+{/* ----------------------------------------------------------------------- */}
+
+  <div className="card-body">
+    <h2 className="card-title">Examinee name: {item.examinee}</h2>
+    <p>Examinee email: {item.examineeemail}</p>
+    <p>Assignment type: {item.type}</p>
+    <p>Assignment name: {item.assignmentname}</p>
+    <div className="card-actions justify-end">
+    
+ <Link to={`/markass/${item._id}`}>
+  <button className="btn btn-primary">Give Mark</button>
+ </Link>
+              
+    </div>
+  </div>
+
+
+  </div>
           ))}
       </div>
     </div>
@@ -35,3 +78,8 @@ const Allsubmission = () => {
 }
 
 export default Allsubmission;
+
+
+
+            
+
---
+++ src/COMPONENTS/Pages/Submissions/pdfpreview.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Pdfpreview= ({item}) => {
+  return (
+    <div>
+
+
+  <body>
+    <h1>PDF Example</h1>
+    <p>Open a PDF file <a href={item}>example</a>.</p>
+  </body>
+
+    </div>
+  );
+};
+
+export default Pdfpreview;
